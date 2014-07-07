41279befa68759d9a7b7c828e1630268af126404
unknown <vuthanhtai92@yahoo.fr>
2014-07-08 01:39:58 +0200

    relation doctrine 1ere partie

---
+++ src/Tatai/Bundle/QcmBundle/Entity/Form.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Tatai\Bundle\QcmBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Form
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Form
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="creation_date", type="datetime")
+     */
+    private $creationDate;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="enabled", type="boolean")
+     */
+    private $enabled;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Form
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set creationDate
+     *
+     * @param \DateTime $creationDate
+     * @return Form
+     */
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get creationDate
+     *
+     * @return \DateTime 
+     */
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param boolean $enabled
+     * @return Form
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return boolean 
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+}
---
+++ src/Tatai/Bundle/QcmBundle/TataiQcmBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tatai\Bundle\QcmBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TataiQcmBundle extends Bundle
+{
+}
